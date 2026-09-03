5601eaf8a7c1869a891ce1008671353619d07d53
kristoffer <kristoffer@spacelab.tech>
2026-09-03 11:09:24 +0200

    Løst oppgave

---
+++ oving3.py
@@ -0,0 +1,28 @@
+from numpy.ma.extras import average
+import pandas as pd
+from entsoe import EntsoePandasClient
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import time
+
+#global variables
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+load_data = pd.read_csv("elk320/elk330/ovingar/power-system-data/load/forbruk_2025.csv",sep=",",header=0,names=["datetime", "Actual Load"],parse_dates=["datetime"])
+
+load_data["datetime"] = pd.to_datetime(load_data["datetime"], utc=True)
+
+#Samler lastdata etter månad, og reikner ut snittlast
+avg_load = load_data.groupby(load_data["datetime"].dt.month)["Actual Load"].mean().round(2)
+
+
+#Skriv til fil
+pd.DataFrame(avg_load.values, index=months, columns=["Gjennomsnittlig last (MW)"]).to_csv("elk320/elk330/ovingar/power-system-data/results/avg_load.csv")
+
+#Plotter resultatet
+fig, ax = plt.subplots()
+ax.plot(months, avg_load.values)
+ax.set_xlabel("Månad")
+ax.set_ylabel("Gjennomsnittlig last (MW)")
+plt.show()
